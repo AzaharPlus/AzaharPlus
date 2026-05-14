20deb230b05f65142764fc7c3fa2dcca87d602ff
AzaharPlus <azahar@plus.3ds>
2026-05-14 21:37:34 +0200

    better licence format

--- src/citra_qt/citra_qt.cpp
+++ src/citra_qt/citra_qt.cpp
@@ -4022,32 +4022,32 @@ void GMainWindow::OnMenuLibzipLicence() {
 \n\
 The authors can be contacted at <info@libzip.org>\n\
 \n\
-Redistribution and use in source and binary forms, with or without\n\
-modification, are permitted provided that the following conditions\n\
+Redistribution and use in source and binary forms, with or without \
+modification, are permitted provided that the following conditions \
 are met:\n\
 \n\
-1. Redistributions of source code must retain the above copyright\n\
-  notice, this list of conditions and the following disclaimer.\n\
+1. Redistributions of source code must retain the above copyright \
+notice, this list of conditions and the following disclaimer.\n\
 \n\
-2. Redistributions in binary form must reproduce the above copyright\n\
-  notice, this list of conditions and the following disclaimer in\n\
-  the documentation and/or other materials provided with the\n\
-  distribution.\n\
+2. Redistributions in binary form must reproduce the above copyright \
+notice, this list of conditions and the following disclaimer in \
+the documentation and/or other materials provided with the \
+distribution.\n\
 \n\
-3. The names of the authors may not be used to endorse or promote\n\
-  products derived from this software without specific prior\n\
-  written permission.\n\
+3. The names of the authors may not be used to endorse or promote \
+products derived from this software without specific prior \
+written permission.\n\
 \n\
-THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS\n\
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n\
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n\
-ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY\n\
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n\
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE\n\
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS\n\
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER\n\
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR\n\
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN\n\
+THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS \
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED \
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE \
+ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY \
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL \
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE \
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS \
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER \
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR \
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN \
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."));
 }
 
